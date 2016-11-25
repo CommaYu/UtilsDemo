225e2014902651aa396f68e9888008b9e54885a6
ysc <215856156@qq.com>
2016-11-26 04:26:45 +0800

    andbase使用Demo

---
+++ andbasetest/src/main/java/com/example/andbasetest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.andbasetest;
+
+import android.os.Bundle;
+
+import com.ab.activity.AbActivity;
+
+public class MainActivity extends AbActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
